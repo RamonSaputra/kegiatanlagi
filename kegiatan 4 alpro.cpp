2036037f460b95b3a59127eb9a3f905726745919
RamonSaputra <muhammad1700018245@webmail.uad.ac.id>
2022-04-13 20:15:45 +0700

    empat

---
+++ kegiatan 4 alpro.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Operator {
+//friend ostream& operator<<(ostream&, Operator&);
+//friend istream& operator>>(istream&, Operator&);
+	public:
+		long faktorial();
+		long faktorial(int n);
+	private:
+		int n;
+		long hasil;
+ };
+
+long Operator::faktorial() {
+	n=7;
+	long fak = 1;
+	for (int i = 1; i<=n; i++){
+		fak = fak * i;
+	}
+return fak;
+
+ }
+ 
+long Operator::faktorial(int n)
+{ 
+	if ((n==0) || (n==1)) {
+		return(1); 
+	}
+	else 
+		return (n*faktorial(n-1));
+ }
+ 
+int main(){
+		Operator run;
+		cout<<run.faktorial();
+		cout<<endl;
+		cout<<run.faktorial(7);
+	
+}
